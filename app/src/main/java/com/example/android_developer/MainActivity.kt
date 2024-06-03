f7e3fc60ecb167dd9b3f9bd103d5cf7515ec6fcf
navneet <navneetpalgithub@gmail.com>
2024-06-03 22:37:58 +0530

    Jupiter

--- app/src/main/java/com/example/android_developer/MainActivity.kt
+++ app/src/main/java/com/example/android_developer/MainActivity.kt
@@ -2,18 +2,24 @@ package com.example.android_developer
 
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import com.example.android_developer.ui.theme.AndroidDeveloperTheme
 import com.example.cryptograph.CryptoSession
 import com.example.cryptograph.CryptoSessionImpl
+import kotlinx.coroutines.launch
 
 
 class MainActivity : ComponentActivity() {
 
-    val session : CryptoSession = CryptoSessionImpl()
+    val session: CryptoSession = CryptoSessionImpl()
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge(
             statusBarStyle = SystemBarStyle.light(
@@ -24,12 +30,34 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AndroidDeveloperTheme {
-
+                Test(session = session)
             }
         }
     }
+}
+
+@Composable
+fun Test(session: CryptoSession) {
+    val coroutineScope = rememberCoroutineScope()
+    Button(
+        onClick = {
+            val key = session.getAESService().generateKey(128)
+            Log.d("key", "key ${session.getAESService().convertKeyToString(key)}")
+            val textToEncrypt = "Hello world I'm here"
+            coroutineScope.launch {
+                val encryptedText = session.getAESService().encryptText(textToEncrypt, key)
+                Log.d("key", "EncryptedText = $encryptedText")
+                val decryptedText = session.getAESService().decryptText(encryptedText!!,key)
+                Log.d("", "DecryptedText = $decryptedText")
+            }
+        }
+    ) {
+        Text(text = "Click")
+    }
 
 }
+
+
 
 
 
--- cryptograph/src/main/java/com/example/cryptograph/aes/AESServiceImpl.kt
+++ cryptograph/src/main/java/com/example/cryptograph/aes/AESServiceImpl.kt
@@ -2,16 +2,19 @@ package com.example.cryptograph.aes
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import java.util.Arrays
 import java.util.Base64
 import javax.crypto.Cipher
 import javax.crypto.KeyGenerator
 import javax.crypto.SecretKey
+import javax.crypto.spec.IvParameterSpec
 import javax.crypto.spec.SecretKeySpec
 
 
 class AESServiceImpl : AESService {
 
     private val symmetricAlgorithm = "AES/CBC/PKCS5PADDING"
+    private val ivSize = 16
     private val keyGenerator: KeyGenerator = KeyGenerator.getInstance("AES")
     private val cipher : Cipher = Cipher.getInstance(symmetricAlgorithm)
 
@@ -30,7 +33,7 @@ class AESServiceImpl : AESService {
     }
 
     override suspend fun encryptText(text: String, key: SecretKey): String? {
-        withContext(Dispatchers.Default){
+        return withContext(Dispatchers.Default){
             cipher.init(Cipher.ENCRYPT_MODE,key)
             val encryptedBytes = try {
                 cipher.doFinal(text.toByteArray())
@@ -40,13 +43,26 @@ class AESServiceImpl : AESService {
             }
             //Here iv stands for Initialisation Vector
             val iv = cipher.iv
+            //Going to extract Iv and encrypted Data
             val combinedIvAndEncryptedData = iv+encryptedBytes
             return@withContext Base64.getEncoder().encodeToString(combinedIvAndEncryptedData)
         }
     }
 
     override suspend fun decryptText(encryptedText: String, key: SecretKey): String? {
-
+        return withContext(Dispatchers.Default){
+            val decodedData = Base64.getDecoder().decode(encryptedText)
+            val iv = Arrays.copyOfRange(decodedData,0,ivSize)
+            val encryptedBytes = Arrays.copyOfRange(decodedData,ivSize,decodedData.size)
+            cipher.init(Cipher.DECRYPT_MODE,key,IvParameterSpec(iv))
+            val decryptedText = try {
+                String(cipher.doFinal(encryptedBytes))
+            }catch (e : Exception){
+                e.printStackTrace()
+                return@withContext null
+            }
+            return@withContext decryptedText
+        }
     }
 
 }
