1ae24a0f06f441dd46b5ccf7c62563fa9babe75c
navneet <navneetpalgithub@gmail.com>
2024-06-03 19:29:45 +0530

    Jupiter

--- cryptograph/src/main/java/com/example/cryptograph/aes/AESService.kt
+++ cryptograph/src/main/java/com/example/cryptograph/aes/AESService.kt
@@ -1,8 +1,12 @@
 package com.example.cryptograph.aes
 
+import javax.crypto.SecretKey
 
 
+interface AESService {
 
-interface AESService{
+    fun generateKey(keyLength: Int): SecretKey
+    fun convertKeyToString(secretKey: SecretKey): String
+    fun convertStringToKey(key: String): SecretKey
 
 }
--- cryptograph/src/main/java/com/example/cryptograph/aes/AESServiceImpl.kt
+++ cryptograph/src/main/java/com/example/cryptograph/aes/AESServiceImpl.kt
@@ -1,8 +1,24 @@
 package com.example.cryptograph.aes
 
-
+import javax.crypto.KeyGenerator
+import javax.crypto.SecretKey
 
 
 class AESServiceImpl : AESService{
+
+    private val keyGenerator : KeyGenerator = KeyGenerator.getInstance("AES")
+
+    override fun generateKey(keyLength: Int): SecretKey {
+        keyGenerator.init(keyLength)
+        return keyGenerator.generateKey()
+    }
+
+    override fun convertKeyToString(secretKey: SecretKey): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun convertStringToKey(key: String): SecretKey {
+        TODO("Not yet implemented")
+    }
 
 }
