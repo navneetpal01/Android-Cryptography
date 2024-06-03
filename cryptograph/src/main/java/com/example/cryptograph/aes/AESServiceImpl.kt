d5cbc441ab99abea53fde334d8dbc9788fbfa5fa
navneet <navneetpalgithub@gmail.com>
2024-06-03 22:09:02 +0530

    Jupiter

--- cryptograph/src/main/java/com/example/cryptograph/aes/AESServiceImpl.kt
+++ cryptograph/src/main/java/com/example/cryptograph/aes/AESServiceImpl.kt
@@ -1,6 +1,9 @@
 package com.example.cryptograph.aes
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import java.util.Base64
+import javax.crypto.Cipher
 import javax.crypto.KeyGenerator
 import javax.crypto.SecretKey
 import javax.crypto.spec.SecretKeySpec
@@ -10,6 +13,7 @@ class AESServiceImpl : AESService {
 
     private val symmetricAlgorithm = "AES/CBC/PKCS5PADDING"
     private val keyGenerator: KeyGenerator = KeyGenerator.getInstance("AES")
+    private val cipher : Cipher = Cipher.getInstance(symmetricAlgorithm)
 
     override fun generateKey(keyLength: Int): SecretKey {
         keyGenerator.init(keyLength)
@@ -26,11 +30,37 @@ class AESServiceImpl : AESService {
     }
 
     override suspend fun encryptText(text: String, key: SecretKey): String? {
-        TODO("Not yet implemented")
+        withContext(Dispatchers.Default){
+            cipher.init(Cipher.ENCRYPT_MODE,key)
+            val encryptedBytes = try {
+                cipher.doFinal(text.toByteArray())
+            }catch (e : Exception){
+                e.printStackTrace()
+                return@withContext null
+            }
+            //Here iv stands for Initialisation Vector
+            val iv = cipher.iv
+            val combinedIvAndEncryptedData = iv+encryptedBytes
+            return@withContext Base64.getEncoder().encodeToString(combinedIvAndEncryptedData)
+        }
     }
 
     override suspend fun decryptText(encryptedText: String, key: SecretKey): String? {
-        TODO("Not yet implemented")
+
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
