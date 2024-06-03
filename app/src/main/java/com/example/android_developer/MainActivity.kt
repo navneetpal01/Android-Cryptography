5b18a3504b32d68d16982d9cb2d58953d33efe8c
navneet <navneetpalgithub@gmail.com>
2024-06-03 18:35:55 +0530

    Jupiter

---
+++ app/src/main/java/com/example/android_developer/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.android_developer
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.android_developer.ui.theme.AndroidDeveloperTheme
+
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                Color.TRANSPARENT,
+                Color.TRANSPARENT
+            )
+        )
+        super.onCreate(savedInstanceState)
+        setContent {
+            AndroidDeveloperTheme {
+
+            }
+        }
+    }
+
+}
+
+
+
