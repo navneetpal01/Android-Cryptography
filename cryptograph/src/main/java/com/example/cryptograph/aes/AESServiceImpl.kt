d0d9316bd7650283a3ffbc1891aa83ed1bae35e7
navneet <navneetpalgithub@gmail.com>
2024-06-04 10:16:00 +0530

    Jupiter

--- cryptograph/src/main/java/com/example/cryptograph/aes/AESServiceImpl.kt
+++ cryptograph/src/main/java/com/example/cryptograph/aes/AESServiceImpl.kt
@@ -2,10 +2,15 @@ package com.example.cryptograph.aes
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import org.bouncycastle.jce.provider.BouncyCastleProvider
 import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.security.Security
 import java.util.Arrays
 import java.util.Base64
 import javax.crypto.Cipher
+import javax.crypto.CipherOutputStream
 import javax.crypto.KeyGenerator
 import javax.crypto.SecretKey
 import javax.crypto.spec.IvParameterSpec
@@ -17,7 +22,7 @@ class AESServiceImpl : AESService {
     private val symmetricAlgorithm = "AES/CBC/PKCS5PADDING"
     private val ivSize = 16
     private val keyGenerator: KeyGenerator = KeyGenerator.getInstance("AES")
-    private val cipher : Cipher = Cipher.getInstance(symmetricAlgorithm)
+    private val cipher: Cipher = Cipher.getInstance(symmetricAlgorithm)
 
     override fun generateKey(keyLength: Int): SecretKey {
         keyGenerator.init(keyLength)
@@ -30,35 +35,35 @@ class AESServiceImpl : AESService {
 
     override fun convertStringToKey(key: String): SecretKey {
         val decodedBytes = Base64.getDecoder().decode(key)
-        return SecretKeySpec(decodedBytes,symmetricAlgorithm)
+        return SecretKeySpec(decodedBytes, symmetricAlgorithm)
     }
 
     override suspend fun encryptText(text: String, key: SecretKey): String? {
-        return withContext(Dispatchers.Default){
-            cipher.init(Cipher.ENCRYPT_MODE,key)
+        return withContext(Dispatchers.Default) {
+            cipher.init(Cipher.ENCRYPT_MODE, key)
             val encryptedBytes = try {
                 cipher.doFinal(text.toByteArray())
-            }catch (e : Exception){
+            } catch (e: Exception) {
                 e.printStackTrace()
                 return@withContext null
             }
             //Here iv stands for Initialisation Vector
             val iv = cipher.iv
             //Going to extract Iv and encrypted Data
-            val combinedIvAndEncryptedData = iv+encryptedBytes
+            val combinedIvAndEncryptedData = iv + encryptedBytes
             return@withContext Base64.getEncoder().encodeToString(combinedIvAndEncryptedData)
         }
     }
 
     override suspend fun decryptText(encryptedText: String, key: SecretKey): String? {
-        return withContext(Dispatchers.Default){
+        return withContext(Dispatchers.Default) {
             val decodedData = Base64.getDecoder().decode(encryptedText)
-            val iv = Arrays.copyOfRange(decodedData,0,ivSize)
-            val encryptedBytes = Arrays.copyOfRange(decodedData,ivSize,decodedData.size)
-            cipher.init(Cipher.DECRYPT_MODE,key,IvParameterSpec(iv))
+            val iv = Arrays.copyOfRange(decodedData, 0, ivSize)
+            val encryptedBytes = Arrays.copyOfRange(decodedData, ivSize, decodedData.size)
+            cipher.init(Cipher.DECRYPT_MODE, key, IvParameterSpec(iv))
             val decryptedText = try {
                 String(cipher.doFinal(encryptedBytes))
-            }catch (e : Exception){
+            } catch (e: Exception) {
                 e.printStackTrace()
                 return@withContext null
             }
@@ -67,7 +72,29 @@ class AESServiceImpl : AESService {
     }
 
     override suspend fun encryptFile(inputFile: File, outputFile: File, key: SecretKey): File? {
-        TODO("Not yet implemented")
+        return withContext(Dispatchers.IO) {
+            return@withContext try {
+                Security.addProvider(BouncyCastleProvider())
+                cipher.init(Cipher.ENCRYPT_MODE, key)
+                val iv = cipher.iv
+                val inputStream = FileInputStream(inputFile)
+                val outputStream = FileOutputStream(outputFile)
+                outputStream.write(iv)
+                val cipherOutputStream = CipherOutputStream(outputStream, cipher)
+                //holds 1024 bytes of data
+                val buffer = ByteArray(1024)
+                var byteRead: Int
+                while (inputStream.read(buffer).also { byteRead = it } != -1) {
+                    cipherOutputStream.write(buffer,0,byteRead)
+                }
+                cipherOutputStream.close()
+                inputStream.close()
+                outputFile
+            } catch (e: Exception) {
+                e.printStackTrace()
+                null
+            }
+        }
     }
 
     override suspend fun decryptFile(encryptedFile: File, outputFile: File, key: SecretKey): File? {
