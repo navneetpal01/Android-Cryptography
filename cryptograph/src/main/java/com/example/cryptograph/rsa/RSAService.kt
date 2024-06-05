c8d73f967747ca80fde11e3d55b4a901cd9d396a
navneet <navneetpalgithub@gmail.com>
2024-06-05 16:59:42 +0530

    Jupiter

--- cryptograph/src/main/java/com/example/cryptograph/rsa/RSAService.kt
+++ cryptograph/src/main/java/com/example/cryptograph/rsa/RSAService.kt
@@ -1,10 +1,23 @@
 package com.example.cryptograph.rsa
 
+import java.security.KeyPair
+import java.security.PrivateKey
+import java.security.PublicKey
 
 
+interface RSAService {
 
+    fun generateRSAKeyPair(keySize: Int): KeyPair
+    fun convertKeyPairToString(keypair: KeyPair): Pair<String, String>
+    fun convertStringToKeyPair(publicKeyString: String, privateKeyString: String): KeyPair
 
+    suspend fun encryptText(text: String, publicKey: PublicKey): String?
+    suspend fun encryptText(text: String, privateKey: PrivateKey): String?
 
-interface RSAService{
+    suspend fun decryptText(decryptText: String, privateKey: PrivateKey): String?
+    suspend fun decryptText(decryptText: String, publicKey: PublicKey): String?
+
 
 }
+
+
--- cryptograph/src/main/java/com/example/cryptograph/rsa/RSAServiceImpl.kt
+++ cryptograph/src/main/java/com/example/cryptograph/rsa/RSAServiceImpl.kt
@@ -1,9 +1,67 @@
 package com.example.cryptograph.rsa
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.security.KeyFactory
+import java.security.KeyPair
+import java.security.KeyPairGenerator
+import java.security.PrivateKey
+import java.security.PublicKey
+import java.security.spec.PKCS8EncodedKeySpec
+import java.security.spec.X509EncodedKeySpec
+import java.util.Base64
 
 
+class RSAServiceImpl : RSAService {
 
-class RSAServiceImpl : RSAService{
+    private val keyPairGenerator = KeyPairGenerator.getInstance("RSA")
+    val keyFactory = KeyFactory.getInstance("RSA")
+
+
+    override fun generateRSAKeyPair(keySize: Int): KeyPair {
+        keyPairGenerator.initialize(keySize)
+        return keyPairGenerator.genKeyPair()
+    }
+
+    override fun convertKeyPairToString(keypair: KeyPair): Pair<String, String> {
+        //Public encoded returns us Byte Array
+        val publicKey = Base64.getEncoder().encodeToString(keypair.public.encoded)
+        val privateKey = Base64.getEncoder().encodeToString(keypair.private.encoded)
+        return publicKey to privateKey
+    }
+
+    override fun convertStringToKeyPair(
+        publicKeyString: String,
+        privateKeyString: String
+    ): KeyPair {
+        val publicKeyBytes = Base64.getDecoder().decode(publicKeyString)
+        val privateKeyBytes = Base64.getDecoder().decode(privateKeyString)
+
+        val publicKeySpec = X509EncodedKeySpec(publicKeyBytes)
+        val privateKeySpec = PKCS8EncodedKeySpec(privateKeyBytes)
+
+        val publicKey = keyFactory.generatePublic(publicKeySpec)
+        val privateKey = keyFactory.generatePrivate(privateKeySpec)
+        return KeyPair(publicKey, privateKey)
+    }
+
+    override suspend fun encryptText(text: String, publicKey: PublicKey): String? {
+        return withContext(Dispatchers.IO){
+            return@withContext ""
+        }
+    }
+
+    override suspend fun encryptText(text: String, privateKey: PrivateKey): String? {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun decryptText(decryptText: String, privateKey: PrivateKey): String? {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun decryptText(decryptText: String, publicKey: PublicKey): String? {
+        TODO("Not yet implemented")
+    }
 
 
 }
