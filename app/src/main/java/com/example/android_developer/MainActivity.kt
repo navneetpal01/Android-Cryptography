9a25d640c09c9df9d5ec97c4f3c60adc4ffc6e01
navneet <navneetpalgithub@gmail.com>
2024-06-07 11:29:55 +0530

    Jupiter

--- app/src/main/java/com/example/android_developer/MainActivity.kt
+++ app/src/main/java/com/example/android_developer/MainActivity.kt
@@ -23,12 +23,9 @@ import com.example.cryptograph.CryptoSession
 import com.example.cryptograph.CryptoSessionImpl
 import com.example.cryptograph.rsa.RSAService
 import com.example.cryptograph.utils.FileHelper
-import com.example.cryptograph.utils.osDownloadDirectory
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import java.io.File
-import java.util.UUID
 
 
 class MainActivity : ComponentActivity() {
@@ -67,29 +64,41 @@ class MainActivity : ComponentActivity() {
                     split[0],
                     "." + split[1]
                 )
-
-                val pathToEncrypt = osDownloadDirectory() + fileName + ".enc"
-                val encryptedFile = File(pathToEncrypt)
-                fileToEncrypt?.let {file ->
-                    //Running on Main thread cause our functions are using withContext to change Threads
+                //Part of Hashing
+                fileToEncrypt?.let { file ->
                     CoroutineScope(Dispatchers.Main).launch {
-                        aesService.encryptFile(file, encryptedFile, aeskey)?.let { encrypted ->
-                            Log.d("file", "Encrypted file path - ${encrypted.path}")
-                            val decryptedOutput = osDownloadDirectory() + "decrypted-${
-                                UUID.randomUUID().toString().substring(0, 4)
-                            }" + fileName
-                            val decryptedOutputFile = File(decryptedOutput)
-                            aesService.decryptFile(
-                                encrypted,
-                                decryptedOutputFile,
-                                aeskey
-                            )?.let {
-                                Log.d("file", "Decrypted file path - ${it.path}")
-                            }
-
-                        }
+                        val textToHash = "Some Random Text To Hash"
+                        val hashedFile = hashService.hash(file)
+                        val hashedText = hashService.hash(textToHash)
+                        Log.d("Hash", "onStart: hashedFile = ${hashedFile.toList()}")
+                        Log.d("Hash", "onStart: hashedText = ${hashedText.toList()}")
                     }
                 }
+
+
+//                //Part of File Encryption
+//                val pathToEncrypt = osDownloadDirectory() + fileName + ".enc"
+//                val encryptedFile = File(pathToEncrypt)
+//                fileToEncrypt?.let { file ->
+//                    //Running on Main thread cause our functions are using withContext to change Threads
+//                    CoroutineScope(Dispatchers.Main).launch {
+//                        aesService.encryptFile(file, encryptedFile, aeskey)?.let { encrypted ->
+//                            Log.d("file", "Encrypted file path - ${encrypted.path}")
+//                            val decryptedOutput = osDownloadDirectory() + "decrypted-${
+//                                UUID.randomUUID().toString().substring(0, 4)
+//                            }" + fileName
+//                            val decryptedOutputFile = File(decryptedOutput)
+//                            aesService.decryptFile(
+//                                encrypted,
+//                                decryptedOutputFile,
+//                                aeskey
+//                            )?.let {
+//                                Log.d("file", "Decrypted file path - ${it.path}")
+//                            }
+//
+//                        }
+//                    }
+//                }
             }
 
         }
@@ -107,8 +116,27 @@ class MainActivity : ComponentActivity() {
         setContent {
             AndroidDeveloperTheme {
 //                Test2(launcher = filePickerLauncher)
-                Test3(service = rsaService)
+//                Test3(service = rsaService)
+                Test4(launcher = filePickerLauncher)
             }
+        }
+    }
+}
+
+@Composable
+fun Test4(launcher: ActivityResultLauncher<String>?) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
+        Button(
+            onClick = {
+                launcher?.launch("*/*")
+            }
+        ) {
+            Text(text = "Click")
         }
     }
 }
@@ -125,9 +153,9 @@ fun Test3(service: RSAService) {
                 val converted = service.convertKeyPairToString(keyPair)
                 Log.d("app", "Here is the private Key ${converted.second}")
                 Log.d("app", "Here is the public Key ${converted.first}")
-                val encryptedText = service.encryptText(textToEncrypt,keyPair.public)
-                encryptedText?.let { 
-                    service.decryptText(it,keyPair.private)?.let {decrypted ->
+                val encryptedText = service.encryptText(textToEncrypt, keyPair.public)
+                encryptedText?.let {
+                    service.decryptText(it, keyPair.private)?.let { decrypted ->
                         Log.d("app", "decrypted Text ${decrypted}")
 
                     }
